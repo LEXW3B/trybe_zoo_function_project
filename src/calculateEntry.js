2e461cca36f3cc8af7b79473b3bfff2b588abee6
Alexandre <alexmanson12345@gmail.com>
2022-07-03 19:47:14 -0300

    Finalizado requesito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const childs = entrants.filter((sum) => sum.age < 18).length;
+  const adults = entrants.filter((sum) => sum.age >= 18 && sum.age < 50).length;
+  const seniors = entrants.filter((sum) => sum.age >= 50).length;
+  return { child: childs, adult: adults, senior: seniors };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const adultPrice = data.prices.adult * countEntrants(entrants).adult;
+  const childPrice = data.prices.child * countEntrants(entrants).child;
+  const seniorPrice = data.prices.senior * countEntrants(entrants).senior;
+  return (adultPrice + childPrice + seniorPrice);
 }
 
 module.exports = { calculateEntry, countEntrants };
