cefe15c4b4d6b6897a3d048d4fca2ef9cf6a350c
Alexandre <alexmanson12345@gmail.com>
2022-07-03 18:33:30 -0300

    Finalizado requesito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui!!!
+function countAnimals(object) {
+  if (!object) {
+    const result = {};
+    data.species.forEach((specie) => {
+      result[specie.name] = specie.residents.length;
+    });
+    return result;
+  }
+  if (object.sex) {
+    return data.species.find((specie) => specie.name === object.specie).residents
+      .filter((resident) => resident.sex === object.sex).length;
+  }
+  return data.species.find((specie) => specie.name === object.specie).residents.length;
 }
 
 module.exports = countAnimals;
